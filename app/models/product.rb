0521efaa0462bfb1ff7d83952d1d054556b74170
meryldakin <meryl.k.dakin@gmail.com>
2017-05-04 08:32:57 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def has_inventory
+    self.inventory == nil || self.inventory < 1 ? false : true
+
+  end
 end
